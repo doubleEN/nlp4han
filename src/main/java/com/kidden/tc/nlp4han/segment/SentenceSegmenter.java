c93dc9bc02e2e146f2d205c06a67fd36c5483a88
kidden <kiddenliu@sina.com>
2017-06-30 15:51:50 +0800

    添加基于规则的中文断句器和相应单元测试。

--- src/main/java/com/kidden/tc/nlp4han/segment/SentenceSegmenter.java
+++ src/main/java/com/kidden/tc/nlp4han/segment/SentenceSegmenter.java
@@ -10,7 +10,7 @@ public interface SentenceSegmenter
     /**
      * 将文本切分为句子
      * 
-     * @param text 待断句的文本
+     * @param text 待断句的文本，一整行，文本中无回车换行
      * @return 句子数组
      */
     public String[] segment(String text);
